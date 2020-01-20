98db8b01e127df8ddcef24d3237291140a6bad06
lamaw01 <janreylamaw@gmail.com>
2020-01-21 00:39:02 +0800

    comments

--- recognizer.py
+++ recognizer.py
@@ -8,7 +8,7 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0);
 recognizer = cv2.face.LBPHFaceRecognizer_create();
 recognizer.read('trainer/trainer.yml');
-flag = 0;
+#flag = 0;
 id=0;
 filename='filename';
 dict = {'item1': 1}
@@ -24,30 +24,30 @@ while True:
         id,conf=recognizer.predict(roi_gray)
         if(conf < 50):
          if(id==1):
-            id='Shubam Walia'
+            id='Janrey'
                            
          elif(id==2):
-            id = 'Rohan'
+            id = 'Sanchez'
     
          elif(id==3):
-            id = 'Raveen'
+            id = 'Ray'
 
          elif(id==4):
-            id = 'Sonu'
+            id = 'Daya'
 
         else:
              id = 'Unknown, can not recognize'
-             flag=flag+1
-             break
+             #flag=flag+1
+             #break
         
         cv2.putText(img,str(id)+" "+str(conf),(x,y-10),font,0.55,(120,255,120),1)
         #cv2.cv.PutText(cv2.cv.fromarray(img),str(id),(x,y+h),font,(0,0,255));
     cv2.imshow('frame',img);
     #cv2.imshow('gray',gray);
-    if flag == 10:
-        playsound('transactionSound.mp3')
-        print("Transaction Blocked")
-        break;
+    #if flag == 10:
+        #playsound('transactionSound.mp3')
+        #print("Transaction Blocked")
+        #break;
     #if time.time()>start+period:
         #break;
     if cv2.waitKey(100) & 0xFF == ord('q'):
